168fd0d51492f9c5f20ea9466d90ab35a488dea5
Abhimanyubhushanmudgal <abhimanyuBmudgal@gmail.com>
2026-02-27 14:37:45 +0530

    UC1: Add welcome message module

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,74 @@
+
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+
+    static void showWelcomeMessage() {
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome Checker ");
+        System.out.println("=================================");
+    }
+
+
+    static void checkHardcodedPalindrome() {
+        String word = "MADAM";
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+
+
+    static void checkUserInputPalindrome() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word or number: ");
+        String input = sc.nextLine();
+
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // UC1
+        showWelcomeMessage();
+
+        // UC2
+        checkHardcodedPalindrome();
+
+        // UC3
+        checkUserInputPalindrome();
+    }
+}
