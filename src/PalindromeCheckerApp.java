438aa9f255b06fb9355885cac9ba98d7b4d72e88
Abhimanyubhushanmudgal <abhimanyuBmudgal@gmail.com>
2026-02-27 15:00:16 +0530

    UC3: User input palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -9,6 +9,29 @@ public class PalindromeCheckerApp {
         System.out.println("   Welcome to Palindrome Checker ");
         System.out.println("=================================");
     }
+    static void checkHardcodedPalindrome() {
+        String word = "MADAM";
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+
 
 
 
@@ -18,6 +41,7 @@ public class PalindromeCheckerApp {
         showWelcomeMessage();
 
         // UC2
+        checkHardcodedPalindrome();
 
     }
 }
