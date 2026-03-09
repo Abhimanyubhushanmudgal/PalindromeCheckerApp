481a4c45728770cbf8af8f44903e1f7b2737be62
sr6062a <rai05111999@gmail.com>
2026-03-09 14:44:50 +0530

    UC8: Linked List based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -59,6 +59,82 @@ public class PalindromeCheckerApp {
             System.out.println(input + " is NOT a Palindrome");
         }
     }
+    // Node class for singly linked list
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Convert string to linked list
+    static Node createLinkedList(String input) {
+        Node head = null, tail = null;
+
+        for (char c : input.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        return head;
+    }
+
+    // Reverse linked list
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    // Check palindrome using linked list
+    static void checkLinkedListPalindrome(String input) {
+
+        Node head = createLinkedList(input);
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        boolean isPalindrome = true;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                isPalindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        if (isPalindrome)
+            System.out.println("Linked List Palindrome");
+        else
+            System.out.println("Not a Linked List Palindrome");
+    }
 
     public static void main(String[] args) {
 
@@ -70,5 +146,7 @@ public class PalindromeCheckerApp {
 
         // UC3
         checkUserInputPalindrome();
+
+        checkLinkedListPalindrome("madam");
     }
 }
