5f73e15c305f4d1c7f99e8a3f11b9063136fa8c3
AbhimanyuBhushanMudgal <abhimanyuBmudgal@gmail.com>
2026-03-09 15:09:50 +0530

    UC8: Linked List based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,27 +4,85 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
 
 
-    static boolean recursivePalindrome(String str, int start, int end) {
+    // Node class for singly linked list
+    static class Node {
+        char data;
+        Node next;
 
-        if (start >= end)
-            return true;
-
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        return recursivePalindrome(str, start + 1, end - 1);
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
     }
 
+    // Convert string to linked list
+    static Node createLinkedList(String input) {
+        Node head = null, tail = null;
 
+        for (char c : input.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        return head;
+    }
+
+    // Reverse linked list
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    // Check palindrome using linked list
+    static void checkLinkedListPalindrome(String input) {
+
+        Node head = createLinkedList(input);
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        boolean isPalindrome = true;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                isPalindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        if (isPalindrome)
+            System.out.println("Linked List Palindrome");
+        else
+            System.out.println("Not a Linked List Palindrome");
+    }
     public static void main(String[] args) {
 
-        String word = "level";
-
-        if(recursivePalindrome(word,0,word.length()-1))
-            System.out.println("Recursive Palindrome");
-        else
-            System.out.println("Not Recursive Palindrome");
-
+        checkLinkedListPalindrome("madam");
 
 
 
