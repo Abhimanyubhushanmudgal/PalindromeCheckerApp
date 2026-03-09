273dad6393011a0814ed909f499338fe53300bf4
AbhimanyuBhushanMudgal <abhimanyuBmudgal@gmail.com>
2026-03-09 15:17:04 +0530

    UC10: Case insensitive and space ignored palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,84 +5,27 @@ public class PalindromeCheckerApp {
 
 
     // Node class for singly linked list
-    static class Node {
-        char data;
-        Node next;
+    static boolean ignoreCaseSpacePalindrome(String input){
 
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
+        input = input.replaceAll("\\s+","").toLowerCase();
 
-    // Convert string to linked list
-    static Node createLinkedList(String input) {
-        Node head = null, tail = null;
+        int start = 0;
+        int end = input.length()-1;
 
-        for (char c : input.toCharArray()) {
-            Node newNode = new Node(c);
+        while(start < end){
 
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
+            if(input.charAt(start) != input.charAt(end))
+                return false;
+
+            start++;
+            end--;
         }
 
-        return head;
-    }
-
-    // Reverse linked list
-    static Node reverse(Node head) {
-        Node prev = null;
-        Node current = head;
-
-        while (current != null) {
-            Node next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        return prev;
-    }
-
-    // Check palindrome using linked list
-    static void checkLinkedListPalindrome(String input) {
-
-        Node head = createLinkedList(input);
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverse(slow);
-        Node firstHalf = head;
-
-        boolean isPalindrome = true;
-
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                isPalindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        if (isPalindrome)
-            System.out.println("Linked List Palindrome");
-        else
-            System.out.println("Not a Linked List Palindrome");
+        return true;
     }
     public static void main(String[] args) {
 
-        checkLinkedListPalindrome("madam");
+        System.out.println(ignoreCaseSpacePalindrome("Never Odd Or Even"));
 
 
 
